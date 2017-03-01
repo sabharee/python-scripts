7a47f97765dc32e16b2373d8911ba03998bcc1e9
sabharee <sabariraj159@gmail.com>
2017-03-02 00:42:47 +0530

    basic python programs

---
+++ python/20170322remove_element_in_list/20170322remove_element_in_list.py
@@ -0,0 +1,10 @@
+nt(raw_input("enter the total number of strings in list:"))
+word=raw_input("enter the strings in list:")
+a=[]
+index=0
+while index<str:
+   next_str=raw_input("enter the string value to add in list:")
+   a.append(next_str)
+   index=index+1
+a.remove(word)
+print a
